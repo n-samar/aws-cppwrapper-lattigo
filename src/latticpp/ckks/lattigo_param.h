89ddd5eb964e74799c1b251c4e7b4cd9c21fb2a4
Nikola Samardzic <nsamar@mit.edu>
2022-06-09 11:36:50 -0400

    [`LattigoParam`] Check that `LattigoParam`s matches what Lattigo expects

--- src/latticpp/ckks/lattigo_param.h
+++ src/latticpp/ckks/lattigo_param.h
@@ -1,9 +1,17 @@
 #pragma once
 
+#include <cassert>
 #include <sstream>
 #include <string>
+#include <unordered_map>
 
 namespace latticpp {
+
+namespace {
+const std::unordered_map<int, int> log_q0_from_bootstrapping_precision{
+    {19, 55}, {25, 60}, {32, 60}};
+
+}  // namespace
 
 class LattigoParam {
  public:
@@ -13,7 +21,20 @@ class LattigoParam {
       : log_N_(log_N),
         log_scale_(log_scale),
         usable_levels_(usable_levels),
-        bootstrapping_precision_(bootstrapping_precision) {}
+        bootstrapping_precision_(bootstrapping_precision) {
+    // These are the only precision our lattigo currently supports
+    assert(bootstrapping_precision_ == 19 || bootstrapping_precision_ == 26 ||
+           bootstrapping_precision_ == 32);
+    int log_q0 =
+        log_q0_from_bootstrapping_precision.at(bootstrapping_precision);
+    // otherwise, the compiler shouts log_q0 is  unused, even tho it is used in
+    // the assert
+    (void)log_q0;
+    // Required by
+    // https://github.com/n-samar/lattigo/blob/master/ckks/bootstrapping/bootstrap.go#L37-L39
+    // (message ratio is 256 in lattigo (= 2^8))
+    assert(log_q0 - 8 >= log_scale_);
+  }
 
   int LogN() const { return log_N_; }
   int LogScale() const { return log_scale_; }
